85a51ebfd02d7f7e90392fe635025ff2358f55f1
jumanahmed <jumanahmedjaki@gmail.com>
2019-03-24 23:47:01 +0600

    Initial commit

---
+++ app/src/main/java/com/example/typesofobservables/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.typesofobservables;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ObservableAndObserver(View view) {
+        startActivity(new Intent(getApplicationContext(), ObserverActivity.class));
+    }
+
+    public void SingleAndSingleObserver(View view) {
+        startActivity(new Intent(getApplicationContext(), SingleObserveActivity.class));
+    }
+}
---
+++ app/src/main/java/com/example/typesofobservables/ObserverActivity.java
@@ -0,0 +1,134 @@
+package com.example.typesofobservables;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class ObserverActivity extends AppCompatActivity {
+
+    private static final String TAG = ObserverActivity.class.getSimpleName();
+    private Disposable disposable;
+
+    /**
+     * Simple Observable emitting multiple Notes
+     * -
+     * Observable : Observer
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_observer);
+
+        Observable<Note> notesObservable = getNotesObservable();
+
+        Observer<Note> notesObserver = getNotesObserver();
+
+        notesObservable.observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(notesObserver);
+    }
+
+
+
+    private Observer<Note> getNotesObserver() {
+        return new Observer<Note>() {
+
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe");
+                disposable = d;
+            }
+
+            @Override
+            public void onNext(Note note) {
+                Log.d(TAG, "onNext: " + note.getNote());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete");
+            }
+        };
+    }
+
+    private Observable<Note> getNotesObservable() {
+        final List<Note> notes = prepareNotes();
+
+        return Observable.create(new ObservableOnSubscribe<Note>() {
+            @Override
+            public void subscribe(ObservableEmitter<Note> emitter) throws Exception {
+                for (Note note : notes) {
+                    if (!emitter.isDisposed()) {
+                        emitter.onNext(note);
+                    }
+                }
+
+                // all notes are emitted
+                if (!emitter.isDisposed()) {
+                    emitter.onComplete();
+                }
+            }
+        });
+    }
+
+    private List<Note> prepareNotes() {
+        List<Note> notes = new ArrayList<>();
+        notes.add(new Note(1, "Buy tooth paste!"));
+        notes.add(new Note(2, "Call brother!"));
+        notes.add(new Note(3, "Watch Narcos tonight!"));
+        notes.add(new Note(4, "Pay power bill!"));
+        return notes;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposable.dispose();
+    }
+
+
+
+    public class Note {
+        int id;
+        String note;
+
+
+        public Note(int id, String note) {
+            this.id = id;
+            this.note = note;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getNote() {
+            return note;
+        }
+
+        public void setNote(String note) {
+            this.note = note;
+        }
+    }
+}
